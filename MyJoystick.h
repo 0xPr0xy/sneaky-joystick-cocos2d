d56852f5dd90be4a9c3cb9713a3e24c832a141b0
0xPr0xy <0xPr0xy@gmail.com>
2012-06-27 10:18:13 +0200

    first commit

---
+++ MyJoystick.h
@@ -0,0 +1,40 @@
+//
+//  MyJoystick.h
+//  Platformer
+//
+//  Created by Michael Colon on 8/13/11.
+//  Copyright 2011 HighBeta, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "SneakyJoystick.h"
+#import "SneakyButton.h"
+#import "SneakyButtonSkinnedBase.h"
+#import "SneakyJoystickSkinnedBase.h"
+
+
+//JOYSTICK AND BUTTON CONSTANTS
+#define JOYSTICK_PAD @"joystickpad.png"
+#define JOYSTICK_BUTTON @"joystickbutton.png"
+#define JUMP_BUTTON @"jumpbutton.png"
+#define JUMP_BUTTON_PRESSED @"jumpbutton_pressed.png"
+#define ATTACK_BUTTON @"firebutton.png"
+#define ATTACK_BUTTON_PRESSED @"firebutton_pressed.png"
+
+
+
+@interface MyJoystick : CCLayer {
+  
+  SneakyJoystick *leftJoystick;
+  SneakyButton *jumpButton;
+  SneakyButton *attackButton;
+    
+}
+
+@property (nonatomic, retain) SneakyJoystick *leftJoystick;
+@property (nonatomic, retain) SneakyButton *jumpButton;
+@property (nonatomic, retain) SneakyButton *attackButton;
+
+
+@end
---
+++ MyJoystick.m
@@ -0,0 +1,66 @@
+//
+//  MyJoystick.m
+//  Platformer
+//
+//  Created by Michael Colon on 8/13/11.
+//  Copyright 2011 HighBeta, LLC. All rights reserved.
+//
+
+#import "MyJoystick.h"
+
+
+@implementation MyJoystick
+
+@synthesize jumpButton;
+@synthesize attackButton;
+@synthesize leftJoystick;
+
+-(id) init {
+  
+  if ( (self = [super init]) ) {
+    
+    self.isTouchEnabled = YES;
+    
+    SneakyJoystickSkinnedBase *leftJoy = [[[SneakyJoystickSkinnedBase alloc] init] autorelease];
+    leftJoy.position = ccp(64,64);
+    
+      
+    leftJoy.backgroundSprite = [CCSprite spriteWithFile:JOYSTICK_PAD];
+    leftJoy.thumbSprite = [CCSprite spriteWithFile:JOYSTICK_BUTTON];
+    leftJoy.joystick = [[SneakyJoystick alloc] initWithRect:CGRectMake(0,0,96,96)];
+    leftJoystick = [leftJoy.joystick autorelease];
+    [self addChild:leftJoy z:0];
+    
+//    SneakyButtonSkinnedBase *jButton = [[[SneakyButtonSkinnedBase alloc] init] autorelease];
+    //jButton.position = ccp(430,50);
+    
+//    jButton.position = ccp(winSize.width - (jButton.contentSize.width) - margin, jButton.contentSize.height + margin);
+//    jButton.defaultSprite = [CCSprite spriteWithFile:JUMP_BUTTON];
+//    jButton.activatedSprite = [CCSprite spriteWithFile:JUMP_BUTTON_PRESSED];
+//    jButton.pressSprite = [CCSprite spriteWithFile:JUMP_BUTTON_PRESSED];
+//    jButton.button = [[SneakyButton alloc] initWithRect:CGRectMake(0, 0, 50, 50)];
+//    [jButton.button setRadius:50.0f];
+//    jumpButton = [jButton.button retain];
+//    jumpButton.isToggleable = NO;
+//    [self addChild:jButton z:0];
+//    
+//    SneakyButtonSkinnedBase *aButton = [[[SneakyButtonSkinnedBase alloc] init] autorelease];
+//    //aButton.position = ccp(430,150);
+//    aButton.position = ccp(winSize.width - (aButton.contentSize.width) - margin, aButton.contentSize.height + margin + 100);
+//    aButton.defaultSprite = [CCSprite spriteWithFile:ATTACK_BUTTON];
+//    aButton.activatedSprite = [CCSprite spriteWithFile:ATTACK_BUTTON_PRESSED];
+//    aButton.pressSprite = [CCSprite spriteWithFile:ATTACK_BUTTON_PRESSED];
+//    aButton.button = [[SneakyButton alloc] initWithRect:CGRectMake(0,0,50,50)];
+//    [aButton.button setRadius:50.0f];
+//    attackButton = [aButton.button retain];
+//    attackButton.isToggleable = NO;
+//    [self addChild:aButton z:0];
+  
+  }
+  
+  return self;
+  
+}
+
+
+@end
